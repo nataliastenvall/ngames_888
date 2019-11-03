9beb7a434d93deaa1c13ce652b618a2a86c3c488
Koroleva Natalia <s1900164@edu.bc.fi>
2019-11-03 23:48:07 +0200

    Updates

---
+++ precache-manifest.530a3f9cf1656e4f08bc9736f7e5818e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8ffe6df4f007a6cb7003a0749d910ac",
+    "url": "/ngames_888/index.html"
+  },
+  {
+    "revision": "133db215c6aa0e581d21",
+    "url": "/ngames_888/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "854eddc1781700c66d83",
+    "url": "/ngames_888/static/css/main.21a52986.chunk.css"
+  },
+  {
+    "revision": "133db215c6aa0e581d21",
+    "url": "/ngames_888/static/js/2.c361bada.chunk.js"
+  },
+  {
+    "revision": "854eddc1781700c66d83",
+    "url": "/ngames_888/static/js/main.c5ed096c.chunk.js"
+  },
+  {
+    "revision": "79a2d569ca58ef4c0ba8",
+    "url": "/ngames_888/static/js/runtime-main.7cf49e69.js"
+  }
+]);
