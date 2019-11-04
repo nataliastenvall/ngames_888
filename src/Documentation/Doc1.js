b59069e8de8ecbadab7185827b32c8e434d9b977
Natalia <natalia.stenvall@gmail.com>
2019-11-04 09:56:24 +0200

    edit

--- src/Documentation/Doc1.js
+++ src/Documentation/Doc1.js
@@ -9,10 +9,10 @@ const Doc1 = props => {
         For the layout, I used react-bootstrap because it is responsive and it
         is an effective way to make a prototype fast. For Datadog status page, I
         used iframe and #past-incidents for showing the list of the latest
-        incidents.For Azure status page I planned to use contentWindow.scrollTo
+        incidents. For Azure status page I planned to use contentWindow.scrollTo
         just React way. And location.reload for refreshing the content. I would
         change my components to class based. So I can use state and set src for
-        my iframe with setInterval. I would call setInterval in
+        my iframe with setInterval every 10min. I would call setInterval in
         componentDidUpdate I suggest. I would also make it more dynamic with
         less repeating in the code. And of course: testing with Jest and Enzyme.
       </p>
