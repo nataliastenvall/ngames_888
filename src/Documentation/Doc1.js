0053c07e1c10fa6349c0b88de9998f02a5ac59b5
Natalia <natalia.stenvall@gmail.com>
2019-11-04 09:53:31 +0200

    Add some idea to docs

--- src/Documentation/Doc1.js
+++ src/Documentation/Doc1.js
@@ -11,8 +11,10 @@ const Doc1 = props => {
         used iframe and #past-incidents for showing the list of the latest
         incidents.For Azure status page I planned to use contentWindow.scrollTo
         just React way. And location.reload for refreshing the content. I would
-        also make it more dynamic with less repeating in the code. And of
-        course: testing with Jest and Enzyme.
+        change my components to class based. So I can use state and set src for
+        my iframe with setInterval. I would call setInterval in
+        componentDidUpdate I suggest. I would also make it more dynamic with
+        less repeating in the code. And of course: testing with Jest and Enzyme.
       </p>
       <hr />
       <p className="mb-0">
